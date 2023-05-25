c2fab7bf335f8cef26ac4545e025a27f719a0951
Lam Truong <thailamtruong05@gmail.com>
2023-05-25 16:11:32 +0700

    fix cors

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = 4000
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-require('./app/config/mongodb.js')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 4000;
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+require("./app/config/mongodb.js");
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // cors
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: [
+      "http://localhost:3000",
+      "https://ecommerce-liard-delta.vercel.app/",
+    ],
     credentials: true,
-  }),
-)
+  })
+);
 // cookie parser
-app.use(cookieParser())
+app.use(cookieParser());
 // routes
 // app.use('/api/auth', require('./app/routes/auth.js'))
 
 // Import routes
-const routes = require('./app/routes')
-app.use('/', routes)
-app.get('/', (req, res) => res.send('Hello World!'))
+const routes = require("./app/routes");
+app.use("/", routes);
+app.get("/", (req, res) => res.send("Hello World!"));
 // Start the server
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
